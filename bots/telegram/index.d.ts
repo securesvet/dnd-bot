43b05c5dc5af87aeef984ae99f61227bdd497f6e
Sviatoslav M. <67125915+securesvet@users.noreply.github.com>
2024-12-11 01:26:12 +0500

    feat: added workspaces && features to be implemented (#5)
    * feat: added workspaces && features to be implemented
    
    * feat: added pre-commit hooks && rearranged helpers
    
    * chore: ran lint
    
    * test: deleted unused hook
    
    * chore: deno lint in pre commit
    
    * chore: added return types
    
    * chore: some changes
    
    * chore: allowing imports
    
    * revert: reverted changes
    
    * chore: deno.yml and deno.json changed
    
    * chore: disabled linting for now (not working --allow-import)

---
+++ bots/telegram/index.d.ts
@@ -0,0 +1,4 @@
+type CommandMainInfo = {
+  command: string;
+  description: string;
+};
--- infrastructure/commands/index.ts
+++ infrastructure/commands/index.ts
@@ -1,1 +1,8 @@
 /* Напоминание о том, что я хочу избежать импорт хелл, поэтому я пока что просто оставлю это здесь */
+
+import type { BaseCommand } from "./BaseCommand.ts";
+import HelloWorldCommand from "./HelloWorldCommand.ts";
+
+export const listOfCommands: BaseCommand[] = [
+  new HelloWorldCommand(),
+];
