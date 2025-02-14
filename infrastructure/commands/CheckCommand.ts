6679b8199801a498da9d319dfd03c927d64a8758
Sviatoslav M. <67125915+securesvet@users.noreply.github.com>
2025-02-14 16:22:22 +0500

    feature: deleted yandex gpt (#26)
    * feat: implemented multiple roll commands
    
    * feat: added emojis and refactored
    
    * feat: added new check feature to talk with AI
    
    * chore: ran fmt
    
    * chore: deleted API
    
    * fix: mixed up boolean
    
    * chore: added gpt
    
    * feat: added OPENAI_API_KEY
    
    * refactor: refactored check command
    
    * refactor: deletetd yandex gpt
    
    * refactor: deletetd yandex gpt

--- infrastructure/commands/CheckCommand.ts
+++ infrastructure/commands/CheckCommand.ts
@@ -1,5 +1,7 @@
+import "jsr:@std/dotenv/load";
 import { BaseCommand } from "./BaseCommand.ts";
 import { getRandomInRange } from "../../helpers/getRandomInRange.ts";
+import { getChatGPTReply } from "../../helpers/getChatGPTReply.ts";
 
 export class CheckCommand extends BaseCommand {
   name: string = "check";
@@ -9,18 +11,34 @@ export class CheckCommand extends BaseCommand {
   private difficulty = getRandomInRange(1, 20);
 
   private userDifficulty: number = getRandomInRange(1, 20);
-  override getReply(): AnswerType[] {
+  override async getReply(): Promise<AnswerType[]> {
     if (this.arguments.length === 0) {
       return [{ text: "Please provide a question." }];
     }
-
+    const adminPrompt =
+      `Ты мастер игры ДНД, тебя спрашивают, сможет ли герой пройти проверку. Красочно опиши. Никаких дополнительных комментариев не требуется.`;
+    const isSuccess = this.difficulty <= this.userDifficulty;
+    const admingPromptSuccessOrFailureText = isSuccess
+      ? "Герой проходит проверку"
+      : "Герой не проходит проверку";
+    let gptReply: string | null;
+    try {
+      gptReply = await getChatGPTReply({
+        adminPrompt: adminPrompt + admingPromptSuccessOrFailureText,
+        userPrompt: this.arguments.join(" "),
+      });
+    } catch (error) {
+      console.error(error);
+      gptReply =
+        "Слушай, я не знаю, У меня закончились мозги, надо подождать немного";
+    }
     return [{
       text:
         `Окей, ты хочешь пройти проверку и узнать, получится ли у тебя:\n<i>${
           this.arguments.join(" ")
         }</i>\nСложность: <b>${this.difficulty}</b>\nУ тебя выпало: <b>${this.userDifficulty}</b>\nРезультат: ${
           this.difficulty <= this.userDifficulty ? "Success" : "Failure"
-        }\nОписание: <b>${"Пока не сделал ИИ сюда, друг"}</b>`,
+        }\nОписание: <b>${gptReply}</b>`,
     }];
   }
 }
