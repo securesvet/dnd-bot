3f46e428ae89ffc18b3db5f976091c91d8b0b67c
Sviatoslav Murzin <mursvyats@gmail.com>
2025-02-14 16:19:54 +0500

    refactor: deletetd yandex gpt

--- infrastructure/commands/CheckCommand.ts
+++ infrastructure/commands/CheckCommand.ts
@@ -2,7 +2,6 @@ import "jsr:@std/dotenv/load";
 import { BaseCommand } from "./BaseCommand.ts";
 import { getRandomInRange } from "../../helpers/getRandomInRange.ts";
 import { getChatGPTReply } from "../../helpers/getChatGPTReply.ts";
-import axios from "npm:axios";
 
 export class CheckCommand extends BaseCommand {
   name: string = "check";
@@ -42,51 +41,5 @@ export class CheckCommand extends BaseCommand {
         }\nОписание: <b>${gptReply}</b>`,
     }];
   }
-  private async getGPTReply(): Promise<string> {
-    const adminPrompt =
-      `Ты мастер игры ДНД, тебя спрашивают, сможет ли герой пройти проверку, сложность проверки: ${this.difficulty}, что выпало у игрока: ${this.userDifficulty}. Красочно опиши процесс того, как получилось у игрока или не получилось совершить действие. Никаких дополнительных комментариев не требуется.`;
-    const url =
-      "https://llm.api.cloud.yandex.net/foundationModels/v1/completion";
-    const data = {
-      modelUri: `gpt://${Deno.env.get("FOLDER_ID") as string}/yandexgpt`,
-      completionOptions: {
-        stream: false,
-        temperature: 0.6,
-        maxTokens: "2000",
-        reasoningOptions: {
-          mode: "DISABLED",
-        },
-      },
-      messages: [
-        {
-          role: "system",
-          text: adminPrompt,
-        },
-        {
-          role: "user",
-          text: this.arguments.join(" "),
-        },
-      ],
-    };
 
-    // Настройки запроса
-    const config = {
-      method: "post",
-      url: url, // Замените на URL вашего API
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${Deno.env.get("IAM_TOKEN") as string}`,
-      },
-      data: data,
-    };
-
-    try {
-      const response = await axios(config);
-      console.log(response.data.result.alternatives[0].message.text);
-      return response.data.result.alternatives[0].message.text;
-    } catch (error) {
-      console.error("Ошибка при вызове API:", error);
-      return "Произошла ошибка при вызове API.";
-    }
-  }
 }
