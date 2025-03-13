7e8d12e9c0bb375c0b53a0379e70d76bf0667bc6
Sviatoslav Murzin <mursvyats@gmail.com>
2025-03-13 22:30:31 +0500

    feat: added new commands and tables

--- infrastructure/commands/MentionAll/MentionAllCommand.ts
+++ infrastructure/commands/MentionAll/MentionAllCommand.ts
@@ -1,22 +1,28 @@
-import { BaseCommand } from "./BaseCommand.ts";
-import type { AnswerType } from "./types.ts";
+import { BaseCommand } from "../BaseCommand.ts";
+import type { AnswerType } from "../types.ts";
 
 export class MentionAllCommand extends BaseCommand {
   protected name = "all";
   protected description = "Command works in group chats, mention all users";
   async getReply(): Promise<AnswerType> {
-    console.log(this.chatInfo);
     if (!this.chatInfo.groupId) {
       console.error("This command is only available in group chats");
       return { text: "This command is only available in group chats" };
     }
-    console.log(this.chatInfo.groupId);
+    const isUserBanned = await this.database.isUserBanned(this.chatInfo.chatId);
+    if (isUserBanned) {
+      console.error("You are banned from using this command");
+      return { text: "You are banned from using this command" };
+    }
     const users = await this.database.getUsersByGroupId(this.chatInfo.groupId);
-    console.log(users);
     if (users === undefined) {
       console.error("No users found");
       return { text: "No users found in this chat" };
     }
-    return { text: `${users.map((user) => `@${user.username}`).join(", ")}` };
+    return {
+      text: `${
+        users.map((user) => `@${user.username}`).join(", ")
+      }\nTo unsubscribe use /unsuball`,
+    };
   }
 }
